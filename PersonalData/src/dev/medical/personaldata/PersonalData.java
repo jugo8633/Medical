7d1c11e4ddb4e7027d0613365f96e916139fb2d8
jugo <jugo.tw@gmail.com>
2014-08-15 13:49:48 +0800

    update

--- PersonalData/src/dev/medical/personaldata/PersonalData.java
+++ PersonalData/src/dev/medical/personaldata/PersonalData.java
@@ -1,23 +1,40 @@
 package dev.medical.personaldata;
 
 import interactive.common.ViewHandler;
+import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class PersonalData
 {
-	private Context				theContext			= null;
 	private OnFinishedListener	onFinishedListener	= null;
+	private Button				btnFinish			= null;
 
 	public static interface OnFinishedListener
 	{
 		public void onFinished();
 	}
 
-	public PersonalData(Context context)
+	public PersonalData(Activity activity)
 	{
 		super();
-		theContext = context;
-		ViewHandler.getResourceId(context, "buttonFinish", "id");
+		int nResId = 0;
+		nResId = ViewHandler.getResourceId(activity, "buttonFinish", "id");
+		btnFinish = (Button) activity.findViewById(nResId);
+		btnFinish.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				if (null != onFinishedListener)
+				{
+					onFinishedListener.onFinished();
+				}
+			}
+		});
+
 	}
 
 	public void setOnFinishedListener(OnFinishedListener listener)
